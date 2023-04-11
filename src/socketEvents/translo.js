38ce5ae74c93af15415fb9c4b728a65fc58bbf82
pendragons-code <pendragonscode@gmail.com>
2023-04-12 00:08:41 +0800

    Støvler på bakken

--- src/socketEvents/translo.js
+++ src/socketEvents/translo.js
@@ -1,18 +1,39 @@
 const axios = require("axios")
 const { db } = require("../loaders/dataBase.js")
 const env = require("dotenv").config()
-// ok here is the thing, I know this is probably a bad method, but dynamic
+let key = process.env.transloAPIKey
 module.exports = {
 	name: "translateRequest",
 	async execute(Input, socket, io) {
 		const keyEngine = await db.get("RequestCounter")
 		if(keyEngine == 10000) return socket.emit("res", { error: "We hit the activity limit! Please try again later!" })
+		if(keyEngine >= 5000) key = process.env.transloAPIKeySecondary
 		const encodedParams = new URLSearchParams()
-		encodedParams.append("from", Input.from)
+		let fromFinal = Input.from
+		if(Input.from === "auto") {
+			let detectLangOptions = {
+				method: "GET",
+				url: 'https://translo.p.rapidapi.com/api/v3/detect',
+				params: {text: Input.text},
+				headers: {
+					"X-RapidAPI-Key": key,
+					"X-RapidAPI-Host": "translo.p.rapidapi.com"
+				}
+
+			}
+			try{
+				let detectLangResponse = await axios.request(detectLangOptions)
+				if(!detectLangResponse) return socket.emit("res", { error: "This service not available now, please try again later!" })
+				if(!detectLangResponse.data.ok) return socket.emit("res", { error: "Invalid fields provided!" })
+				await db.add("RequestCounter", 1)
+				fromFinal = detectLangResponse.data.lang
+			} catch(e) {
+				return socket.emit("res", "AN ERROR OCCURED!!!")
+			}
+		}
+		encodedParams.append("from", fromFinal)
 		encodedParams.append("to", Input.to)
 		encodedParams.append("text", Input.text)
-		let key = process.env.transloAPIKey
-		if(keyEngine >= 5000) key = process.env.transloAPIKeySecondary
 		const options = {
 			method: "POST",
 			url: "https://translo.p.rapidapi.com/api/v3/translate",
@@ -32,6 +53,5 @@ module.exports = {
 		} catch(e) {
 			return socket.emit("res", { error: "Invalid fields provided!" })
 		}
-		//should use the json file from assets and do a .includes check, but im too lazy now.
 	}
 }
