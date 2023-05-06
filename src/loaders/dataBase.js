73dc5636ee683caddbb63155d0006f23129ab0e0
pendragons-code <pendragonscode@gmail.com>
2023-05-07 01:45:52 +0800

    murder me

--- src/loaders/dataBase.js
+++ src/loaders/dataBase.js
@@ -1,3 +1,9 @@
 const { QuickDB } = require("quick.db")
 const db = new QuickDB({ filePath: "DataBase/db.sqlite" })
+
+// Did this out of habit because I like to separate the database init from the webserver.
+// Usually I would use something like mongodb and cassandra.
+//
+// Not gonna lie if people really want me to combine socketManager.js, frontEnd.js, and dataBase.js into webserver.js. I will. So far I have only been getting questions.
+// That said, I do agree that having an app.js file hanging on is really stupid.
 module.exports = { db }
--- src/loaders/socketManager.js
+++ src/loaders/socketManager.js
@@ -6,7 +6,7 @@ const io = socket(server)
 const loadSocketEvents = readdirSync("./src/socketEvents").filter(files => files.endsWith(".js"))
 io.on("connection", (socket) => {
 	for(const file of loadSocketEvents) {
-		const { name, execute} = require(`../socketEvents/${file}`)
+		const { name, execute } = require(`../socketEvents/${file}`)
 		socket.on(name, (Input) => {
 			execute(Input, socket, io)
 			// input should have a json like structure
